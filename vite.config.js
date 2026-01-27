d00902ac750b5abaa801f0d0d6aff3664ad8cfb0
Mafifame <mafifame@gmail.com>
2026-01-27 03:46:56 +0100

    build for github pages

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,10 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: process.env.NODE_ENV === "production" ? "/tineportfolio.com/" : "/",
+build: {
+    outDir: "docs"
+  },
+
   plugins: [react()],
 })
